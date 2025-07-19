c19bab3e24a6391fdb200fdde188c3b25dbbbb3b
GAURAV KUMAR SHUKLA <GauravShukla092@gmail.com>
2025-07-19 19:28:01 -0400

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/equinoxqa/ai-playwright-docs/edit/main/',
         },
         blog: false,
         theme: {
@@ -67,7 +67,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'AI-Powered Test Automation',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -77,10 +77,10 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'ai-playwright',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/equinoxqa/ai-playwright-docs',
             label: 'GitHub',
             position: 'right',
           },
@@ -125,7 +125,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} equinoxqa Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
